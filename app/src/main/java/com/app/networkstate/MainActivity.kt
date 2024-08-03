8b90608932eb79378cb07a4460a3f96325c64a5b
navneet <navneetpalgithub@gmail.com>
2024-08-03 11:26:21 +0530

    Network Manager

--- app/src/main/java/com/app/networkstate/MainActivity.kt
+++ app/src/main/java/com/app/networkstate/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.app.networkstate
 
+import android.content.Context
 import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.app.networkstate.ui.theme.NetworkStateTheme
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +52,28 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@Composable
+fun NetworkScreen(){
+    val connectionState by rememberConnectivityState()
+    val isConnected by remember(connectionState){
+        derivedStateOf {
+            connectionState == NetworkConnectionState.Available
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = if (isConnected) "Connected" else "Unavailable",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+}
+
 sealed interface NetworkConnectionState {
     data object Available : NetworkConnectionState
     data object Unavailable : NetworkConnectionState
@@ -51,7 +93,51 @@ private fun networkCallback(callback: (NetworkConnectionState) -> Unit): Connect
 
 fun getCurrentConnectivityState(connectivityManager: ConnectivityManager): NetworkConnectionState {
     val network = connectivityManager.activeNetwork
+    val isConnected = connectivityManager
+        .getNetworkCapabilities(network)
+        ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+
+    return if (isConnected) NetworkConnectionState.Available else NetworkConnectionState.Unavailable
 }
+
+fun Context.observeConnectivityAsFlow(): Flow<NetworkConnectionState> {
+    return callbackFlow {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val callback = networkCallback { connectionState ->
+            trySend(connectionState)
+        }
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, callback)
+        val currentState = getCurrentConnectivityState(connectivityManager)
+        trySend(currentState)
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+    }
+
+}
+
+
+val  Context.currentConnectivityState : NetworkConnectionState
+    get(){
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return getCurrentConnectivityState(connectivityManager)
+    }
+
+@Composable
+fun rememberConnectivityState() : State<NetworkConnectionState>{
+    val context = LocalContext.current
+    return produceState(initialValue = context.currentConnectivityState) {
+        context.observeConnectivityAsFlow().collect{
+            value = it
+        }
+    }
+}
+
 
 
 
